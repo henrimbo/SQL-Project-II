6b1d495985133fa27f6eb3e8e3ea13f07a0a3f4c
Henrique Borges <144286173+henrimbo@users.noreply.github.com>
2024-01-23 18:25:44 +0800

    Update SQL Queries.sql

--- SQL Queries.sql
+++ SQL Queries.sql
@@ -2,19 +2,25 @@
 
  SELECT * 
  FROM `bigquery-public-data.chicago_taxi_trips.taxi_trips`
- WHERE trip_seconds > 1200 AND tips > 0 
+ WHERE trip_seconds > 1200 AND 
+       tips > 0 
 
 -- # Count of trips with tolls, grouped by company and specific date: 
 
- SELECT company, FORMAT_TIMESTAMP('%y-%m-%d', trip_start_timestamp) AS data_trip, 
-   COUNT (unique_key) AS Total_trips
+ SELECT company, 
+        FORMAT_TIMESTAMP('%y-%m-%d', trip_start_timestamp) AS data_trip, 
+        COUNT (unique_key) AS Total_trips
  FROM `bigquery-public-data.chicago_taxi_trips.taxi_trips`
- WHERE tolls > 0 AND FORMAT_TIMESTAMP('%y-%m-%d', trip_start_timestamp) = '15-08-21'
- GROUP BY company, data_trip
+ WHERE tolls > 0 AND 
+       FORMAT_TIMESTAMP('%y-%m-%d', trip_start_timestamp) = '15-08-21'
+ GROUP BY company, 
+          data_trip
 
 -- # Count of trips per year for a specific company:
 
- SELECT company, EXTRACT(YEAR FROM trip_start_timestamp) as Year, COUNT (unique_key) AS Total_trips
+ SELECT company, 
+        EXTRACT(YEAR FROM trip_start_timestamp) as Year, 
+        COUNT (unique_key) AS Total_trips
  FROM `bigquery-public-data.chicago_taxi_trips.taxi_trips`
  WHERE company = 'Taxi Affiliation Services'
  GROUP BY company, Year
@@ -23,27 +29,31 @@
 
 -- # Analysing the average price of the trips over the year
 
- SELECT EXTRACT(YEAR FROM trip_start_timestamp) AS year, AVG(fare) AS average_fare
+ SELECT EXTRACT(YEAR FROM trip_start_timestamp) AS year, 
+        AVG(fare) AS average_fare
  FROM `bigquery-public-data.chicago_taxi_trips.taxi_trips`
  GROUP BY year
 
 
  -- # Analysing the total miles of the trips by year and company. 
 
- SELECT company, EXTRACT(YEAR FROM trip_start_timestamp) AS year, SUM(trip_miles) AS total_miles
+ SELECT company, 
+        EXTRACT(YEAR FROM trip_start_timestamp) AS year, 
+        SUM(trip_miles) AS total_miles
  FROM `bigquery-public-data.chicago_taxi_trips.taxi_trips`
  GROUP BY company, year
  ORDER BY company, year
 
  -- # Analysing the payment type by year and company
 
- SELECT 
-   company, 
-   EXTRACT(YEAR FROM trip_start_timestamp) AS year, 
-   payment_type, 
-   COUNT(unique_key) AS total_trips, 
+ SELECT company, 
+        EXTRACT(YEAR FROM trip_start_timestamp) AS year, 
+        payment_type, 
+        COUNT(unique_key) AS total_trips, 
  FROM `bigquery-public-data.chicago_taxi_trips.taxi_trips`
- GROUP BY company, year, payment_type
+ GROUP BY company, 
+          year, 
+          payment_type
 
 
  -- # Conditional column for fare ranges and analysis of payment type by fare range:
@@ -55,26 +65,26 @@
      WHEN fare BETWEEN 20 AND 30 THEN '20-30'
      ELSE '30+' 
    END AS fare_range,
-   payment_type
+          payment_type
  FROM `bigquery-public-data.chicago_taxi_trips.taxi_trips`
 
  -- # Analysing the average price of the trips with a distance above 10 miles
 
  SELECT company, 
-   AVG(fare) AS average_fare
+        AVG(fare) AS average_fare
  FROM `bigquery-public-data.chicago_taxi_trips.taxi_trips`
  WHERE trip_miles > 10
  GROUP BY company
 
  -- # Analysing average price, distance, quantity (number of trips), time of trips by year and company.  
 
- SELECT 
-   company, 
-   EXTRACT(YEAR FROM trip_start_timestamp) AS year, 
-   AVG(fare) AS average_fare, 
-   SUM(trip_miles) AS total_miles, 
-   COUNT(unique_key) AS total_trips, 
-   SUM(trip_seconds) / 3600 AS total_hours
+ SELECT company, 
+        EXTRACT(YEAR FROM trip_start_timestamp) AS year, 
+        AVG(fare) AS average_fare, 
+        SUM(trip_miles) AS total_miles, 
+        COUNT(unique_key) AS total_trips, 
+        SUM(trip_seconds) / 3600 AS total_hours
  FROM `bigquery-public-data.chicago_taxi_trips.taxi_trips`
- GROUP BY company, year
+ GROUP BY company, 
+          year
 
